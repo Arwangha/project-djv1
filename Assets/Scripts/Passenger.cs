db1883a0a287ede991328cf6bb41f56f9bb53ac4
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-01-09 13:03:59 +0100

    Passenger spawners

--- Assets/Scripts/Passenger.cs
+++ Assets/Scripts/Passenger.cs
@@ -5,13 +5,22 @@ using UnityEngine.AI;
 public class Passenger : Entity
 {
     public bool fraudeur;
+    public float fraudLikelihood;
     public bool hasPassedGate;
     public Vector3 target;
     private NavMeshAgent _navMeshAgent;
+    private bool _busy;
 
     private void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        fraudeur = Random.value > fraudLikelihood;
+        target = transform.position;
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        this.target = target;
     }
 
     public bool Control()
@@ -19,9 +28,12 @@ public class Passenger : Entity
         return fraudeur;
     }
 
-    public void MoveForward()
+    public IEnumerator MoveForward()
     {
+        _busy = true;
         _navMeshAgent.destination = transform.position + transform.forward;
+        yield return new WaitForSeconds(1f);
+        _busy = false;
     }
 
     private void Update()
@@ -29,6 +41,10 @@ public class Passenger : Entity
         if (hasPassedGate)
         {
             _navMeshAgent.destination = target;
+        }
+        else if(!_busy)
+        {
+            _navMeshAgent.destination = transform.position;
         }
     }
 }
--- Assets/Scripts/Tourniquet.cs
+++ Assets/Scripts/Tourniquet.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Tourniquet : MonoBehaviour
 {
     [SerializeField] private GameObject passengerPrefab;
     [SerializeField] private float validationTime;
+    [SerializeField] private float maxTargetX;
+    [SerializeField] private float maxTargetZ;
+    [SerializeField] private float minTargetX;
+    [SerializeField] private float minTargetZ;
     private List<Passenger> _passengers = new List<Passenger>();
     private bool _busy;
     void Start()
     {
-        
+        AddPassenger(3);
     }
 
     // Update is called once per frame
@@ -19,12 +24,37 @@ public class Tourniquet : MonoBehaviour
         if (!_busy) StartCoroutine(Validation());
     }
 
-    private IEnumerator Validation()
+    private void AddPassenger(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Passenger passenger = Instantiate(passengerPrefab, transform.position, Quaternion.identity).GetComponent<Passenger>();
+            passenger.transform.gameObject.SetActive(true);
+            passenger.SetTarget( 
+                new Vector3(Random.Range(minTargetX, maxTargetX), 
+                transform.position.y, 
+                Random.Range(minTargetZ, maxTargetZ)));
+            _passengers.Add(passenger);
+            MoveEveryOneForward();
+        }
+    }
+
+    private void MoveEveryOneForward()
     {
         foreach (var guy in _passengers)
         {
-            guy.MoveForward();
+            StartCoroutine(guy.MoveForward());
         }
+    }
+
+    private IEnumerator Validation()
+    {
+        _busy = true;
         yield return new WaitForSeconds(validationTime);
+        Passenger passenger = _passengers.ElementAt(0);
+        passenger.hasPassedGate = true;
+        _passengers.Remove(passenger);
+        AddPassenger(1);
+        _busy = false;
     }
 }
