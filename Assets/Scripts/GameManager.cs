90c3973abc40a75938d55b9fc61a3f370433a8f4
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-01-10 08:51:15 +0100

    Working menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
+    public void StartLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    
     // Update is called once per frame
     void Update()
     {
